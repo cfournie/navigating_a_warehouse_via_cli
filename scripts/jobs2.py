f65486204e7b32807de654d52ecb1a809ff5b3c0
Chris Fournier <chris.fournier@shopify.com>
2017-11-04 20:24:37 -0400

    Add sigpipe handling script and fix lint errors

--- scripts/jobs2.py
+++ scripts/jobs2.py
@@ -1,7 +1,7 @@
 import navigate_warehouse_via_cli.lib as lib
 
-# pylint: disable=invalid-name
 
+# pylint: disable=invalid-name,duplicate-code
 if __name__ == '__main__':
     flows = lib.generate_schedule(seed=2)
     for flow in flows.values():
--- scripts/jobs4.py
+++ scripts/jobs4.py
@@ -1,8 +1,18 @@
+import signal
+import sys
+
 import navigate_warehouse_via_cli.lib as lib
 
 
-# pylint: disable=invalid-name,duplicate-code
+# pylint: disable=invalid-name
 if __name__ == '__main__':
+    # Handle the SIGPIPE signal by exiting gracefully when receiving it. This
+    # occurs when our output is piped to `head`
+    def handle_sigpipe(_, __):
+        sys.exit(0)
+    signal.signal(signal.SIGPIPE, handle_sigpipe)
+
+    # Output jobs
     flows = lib.generate_schedule(seed=2)
     for flow in flows.values():
         for name, job in flow.jobs.items():
