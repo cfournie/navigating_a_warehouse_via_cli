74b480bf78b015626ca67d9d1f17d077b50973c6
Chris Fournier <chris.fournier@shopify.com>
2017-11-11 21:34:19 -0500

    Refactor and reduce slides

--- scripts/broken_jobs.py
+++ scripts/broken_jobs.py
@@ -1,8 +1,11 @@
+import sys
+
 import navigate_warehouse_via_cli.lib as lib
 
 
-# pylint: disable=invalid-name,duplicate-code
+# pylint: disable=invalid-name
 if __name__ == '__main__':
+    # Write table to stdout
     flows = lib.generate_schedule(seed=2)
     for flow in flows:
         for job in flow.jobs:
